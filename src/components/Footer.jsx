a55cf4a1592fd5f300d65aecf608d837125f697c
Rajat Guptat <rajatgupta.tech@gmail.com>
2022-02-02 17:33:22 +0530

    edited

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import react from "react";
+
+function Footer(){
+    const currentYear = new Date().getFullYear();
+    return (
+        <footer>
+            <p>
+              Copyright ⓒ {currentYear}
+            </p>
+        </footer>
+    )
+}
+
+export default Footer;
